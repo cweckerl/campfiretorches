237a447fb212d24864f80b3584007bca1c496ae9
Enemeez <c4rl.hawks@gmail.com>
2019-08-09 22:50:55 -0500

    update 1.0.0

---
+++ src/main/java/enemeez/campfiretorches/CampfireTorches.java
@@ -0,0 +1,10 @@
+package enemeez.campfiretorches;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("campfiretorches")
+public class CampfireTorches
+{
+    public static final String MOD_ID = "campfiretorches";
+      
+}
